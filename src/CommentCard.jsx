ecae3dfd830bb6a833f090d7332815d06651637c
HarryHadcroft <harry.hadcroft@delmdigital.co.uk>
2024-03-12 15:24:52 +0000

    Added a delete button to the users comments

--- src/CommentCard.jsx
+++ src/CommentCard.jsx
@@ -1,15 +1,26 @@
+import { useContext } from "react";
+import { UserContext } from "./contexts/User";
+
 export const CommentCard = ({ comments }) => {
-    return (
-        <section>
-            {comments.map((comment) => {
-                return(
-                    <div key={comment.comment_id}className="comment-card">
-                        <p>{comment.author}</p>
-                        <p>{comment.body}</p>
-                        <p>votes {comment.votes}</p>
-                    </div>
-                )
-            })}
-        </section>
-    )
-}
+  const { loggedInUser } = useContext(UserContext);
+  return (
+    <section>
+      {comments.map((comment) => {
+        const canDelete = comment.author === loggedInUser.username;
+
+        return (
+          <div key={comment.comment_id} className="comment-card">
+            <p>{comment.author}</p>
+            <p>{comment.body}</p>
+            <p>votes {comment.votes}</p>
+            {canDelete && (
+              <button onClick={() => handleDelete(comment.comment_id)}>
+                Delete comment
+              </button>
+            )}
+          </div>
+        );
+      })}
+    </section>
+  );
+};
